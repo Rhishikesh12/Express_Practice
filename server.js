f4371779bfbdc75720b2d97c187a948607c5fa40
Rhishikesh12 <rhishikeshchaughule@gmail.com>
2023-07-15 18:22:54 +0530

    pushing " Ejs "

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+// Using Template Engine
+// { views } is default folder in ejs
+app.set("view engine", "ejs");
+
+// to print views path where we can save dynamic pages
+// console.log(app.get("views"));
+
 app.get("/", (req, res) => {
-	res.send("Hello World");
+	res.render("index", {
+		title: "Home Page",
+	});
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "About Page",
+	});
 });
 
 const PORT = process.env.PORT || 5000;
